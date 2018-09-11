4a081c6897b27efde5c2ce81ce4944963f66a3a9
Carlos Rivera <carlosrdev@gmail.com>
2018-09-10 23:45:09 -0500

    Create ejemplophp1.php

---
+++ ejemplophp1.php
@@ -0,0 +1,29 @@
+
+<?php //Necesitamos este para declarar que usamos php
+
+$edad=38;  //Usamos $ para declarar variables 
+$nombre="Victor";  
+$clave="84644";
+
+//recuperamos el valor de nuestra primera pagina
+$name=$_POST['nombre'];
+
+echo ("El nombre es $nombre");
+
+echo ("La edad es $edad");
+
+//Usamos "" para lanzar el codigo automaticamente
+echo("La clave es $clave");
+
+echo("Bienvenido $name");
+//Usamos comillas simples para compilar algo por ejemplo
+//codigo de algo 
+echo('<!DOCTYPE html>
+<html>
+<head>
+</head>
+<body>
+</body>
+</html>')
+
+?>

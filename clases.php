7921bcba0633fad0330855e58c6c5c0ee41e47f0
Carlos Rivera <carlosrdev@gmail.com>
2018-09-11 00:29:29 -0500

    Create clases.php

---
+++ clases.php
@@ -0,0 +1,16 @@
+<?php 
+//Creamos una clase
+//Podemos declarar cualquier numero de clases
+	class Miclase {
+		//Atributos
+
+		//Metodos
+	}
+
+	class Miclase2 {
+		//Atributos
+
+		//Metodos
+	}
+
+ ?>

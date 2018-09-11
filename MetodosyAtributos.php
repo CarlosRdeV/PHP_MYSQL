ce882c84745ccfbf7b240f973c3da655a652d1f1
Carlos Rivera <carlosrdev@gmail.com>
2018-09-11 00:30:46 -0500

    Create MetodosyAtributos.php

---
+++ MetodosyAtributos.php
@@ -0,0 +1,18 @@
+<?php 
+
+	class unaClase{
+		//declaracion de atributo
+		public $atributo1="";
+
+
+		//declaracion de metodos
+		public function miMetodo(){ 
+			//como podemos mandar a llamar un atributo
+			//utilizando la pseudo variable this
+
+
+			//todos los metodos llevan un return?	
+			return $this->atributo1;
+		}
+	}
+?>
